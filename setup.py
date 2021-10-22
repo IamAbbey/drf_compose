e2df1c7b1478bf2eb2db2edc8a4dd0780ba9077d
Sotunde Abiodun <sotundeabiodun00@gmail.com>
2021-10-22 19:43:08 +0100

    improvements made

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import find_packages, setup
 
-with open("README.rst") as readme_file:
+with open("READMEPY.rst") as readme_file:
     readme = readme_file.read()
 
 with open("HISTORY.rst") as history_file:
@@ -47,7 +47,7 @@ setup(
     },
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     include_package_data=True,
     keywords="drf_compose",
     name="drf_compose",
