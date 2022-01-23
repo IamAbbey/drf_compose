aaf2ab5c805510d163619f24449bc6e8fa6a1789
Sotunde Abiodun <sotundeabiodun00@gmail.com>
2022-01-23 22:23:56 +0100

    test coverage improved to 100%

--- tests/test_drf_compose.py
+++ tests/test_drf_compose.py
@@ -6,97 +6,18 @@ import pytest
 from click.testing import CliRunner
 
 from drf_compose import cli
+from .test_compose_contents import json_test_compose, yaml_test_compose
 
-test_compose = """
-{
-    "name": "delight_blog",
-    "app_with_model": [
-      {
-        "app_name": "post",
-        "models": [
-          {
-            "name": "Post",
-            "meta": {
-              "verbose_name": "Post",
-              "verbose_name_plural": "Posts",
-              "ordering": ["-content"]
-            },
-            "use_uuid_as_key": true,
-            "fields": [
-              {
-                "name": "title",
-                "type": "char",
-                "options": {
-                  "blank": true,
-                  "null": true,
-                  "max_length": 200
-                }
-              },
-              {
-                "name": "content",
-                "type": "text",
-                "options": {
-                  "blank": true,
-                  "null": true
-                }
-              }
-            ],
-            "str": "title"
-          }
-        ]
-      }
-    ],
-    "auth_app": {
-        "app_name": "authentication",
-        "model_name": "CustomUser",
-        "meta": {
-            "verbose_name": "CustomUser",
-            "verbose_name_plural": "CustomUsers",
-            "ordering": ["-id"]
-        },
-        "username_field": "email",
-        "required_fields": ["phone"],
-        "use_uuid_as_key": true,
-        "fields": [
-            {
-                "name": "title",
-                "type": "char",
-                "options": {
-                "blank": true,
-                "null": true,
-                "max_length": 200
-                }
-            },
-            {
-                "name": "email",
-                "type": "email",
-                "options": {
-                "unique": true,
-                "max_length": 200
-                }
-            },
-            {
-                "name": "phone",
-                "type": "char",
-                "options": {
-                "max_length": 15,
-                "blank": true,
-                "null": true
-                }
-            }
-        ],
-        "str": "title"
-    }
-  }
-"""
+def create_compose_file(content=None, use_json = True):
+    if use_json:
+        pathlib.Path("drf-compose.json").touch(exist_ok=True)
+        pathlib.Path("drf-compose.json").write_text(content if content else json_test_compose)
+    else:
+        pathlib.Path("drf-compose.yaml").touch(exist_ok=True)
+        pathlib.Path("drf-compose.yaml").write_text(content if content else yaml_test_compose)
 
 
-def create_compose_file(content=None):
-    pathlib.Path("drf-compose.json").touch(exist_ok=True)
-    pathlib.Path("drf-compose.json").write_text(content if content else test_compose)
-
-
-def test_valid_compose_file():
+def test_valid_json_compose_file():
     """Test the CLI."""
     runner = CliRunner()
     with runner.isolated_filesystem():
@@ -104,6 +25,32 @@ def test_valid_compose_file():
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
 
+def test_valid_yaml_compose_file():
+    """Test the CLI."""
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        create_compose_file(use_json=False)
+        result = runner.invoke(cli.main, args="-s drf-compose.yaml  --yaml")
+        assert result.exit_code == 0
+
+def test_project_folder_already_exist():
+    """Test the CLI."""
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        create_compose_file()
+        result = runner.invoke(cli.main)
+        result = runner.invoke(cli.main)
+        assert result.exception
+        assert result.exit_code == 1
+
+def test_error_parsing_compose_file():
+    """Test the CLI."""
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        create_compose_file(content="{")
+        result = runner.invoke(cli.main)
+        assert result.exception
+        assert result.exit_code == 1
 
 def test_missing_compose_file():
     """Test the CLI."""
@@ -126,7 +73,7 @@ def test_missing_compose_file():
 def test_missing_level_1_required_key(key_to_remove, exit_code):
     runner = CliRunner()
     with runner.isolated_filesystem():
-        test_compose_json: dict = json.loads(test_compose)
+        test_compose_json: dict = json.loads(json_test_compose)
         test_compose_json.pop(key_to_remove, None)
         create_compose_file(json.dumps(test_compose_json))
         result = runner.invoke(cli.main)
@@ -148,7 +95,7 @@ def test_missing_level_1_required_key(key_to_remove, exit_code):
 def test_level_2_required_key(parent_key, parent_is_list, key_to_remove, exit_code):
     runner = CliRunner()
     with runner.isolated_filesystem():
-        test_compose_json: dict = json.loads(test_compose)
+        test_compose_json: dict = json.loads(json_test_compose)
         if parent_is_list:
             test_compose_json[parent_key][0].pop(key_to_remove, None)
         else:
@@ -177,7 +124,7 @@ def test_level_3_required_key(
 ):
     runner = CliRunner()
     with runner.isolated_filesystem():
-        test_compose_json: dict = json.loads(test_compose)
+        test_compose_json: dict = json.loads(json_test_compose)
         if ancestor_is_list:
             ancestor = test_compose_json[ancestor_key][0]
         else:
@@ -185,7 +132,7 @@ def test_level_3_required_key(
         if parent_is_list:
             ancestor[parent_key][0].pop(key_to_remove, None)
         else:
-            ancestor[parent_key].pop(key_to_remove, None)
+            ancestor[parent_key].pop(key_to_remove, None) # pragma: no cover
         create_compose_file(json.dumps(test_compose_json))
         result = runner.invoke(cli.main)
         print(result.stdout)
@@ -215,19 +162,19 @@ def test_level_4_required_key(
 ):
     runner = CliRunner()
     with runner.isolated_filesystem():
-        test_compose_json: dict = json.loads(test_compose)
+        test_compose_json: dict = json.loads(json_test_compose)
         if grand_ancestor_is_list:
             grand_ancestor = test_compose_json[grand_ancestor_key][0]
         else:
-            grand_ancestor = test_compose_json[grand_ancestor_key]
+            grand_ancestor = test_compose_json[grand_ancestor_key] # pragma: no cover
         if ancestor_is_list:
             ancestor = grand_ancestor[ancestor_key][0]
         else:
-            ancestor = grand_ancestor[ancestor_key]
+            ancestor = grand_ancestor[ancestor_key] # pragma: no cover
         if parent_is_list:
             ancestor[parent_key][0].pop(key_to_remove, None)
         else:
-            ancestor[parent_key].pop(key_to_remove, None)
+            ancestor[parent_key].pop(key_to_remove, None) # pragma: no cover
         create_compose_file(json.dumps(test_compose_json))
         result = runner.invoke(cli.main)
         if exit_code == 1:
