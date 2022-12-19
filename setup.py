340716bc16d0a46043100a68372e8409331fc96a
Sotunde Abiodun <sotundeabiodun00@gmail.com>
2022-12-19 23:39:34 +0100

    fix build error

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    "Click==8.0",
+    "Click==8.1.3",
     "Django==4.0.6",
     "django-cors-headers==3.13.0",
-    "djangorestframework==3.13.0",
+    "djangorestframework==3.13.1",
     "drf-yasg==1.21.3",
     "emoji==2.0.0",
     "PyYAML==6.0",
-    "black==21.12b0",
+    "black==22.12.0",
 ]
 
 test_requirements = [
@@ -28,16 +28,13 @@ test_requirements = [
 setup(
     author="Sotunde Abiodun",
     author_email="sotundeabiodun00@gmail.com",
-    python_requires=">=3.6",
+    python_requires=">=3.10",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
     ],
     description="DRF Compose generates ready to use API using Django Rest Framework",
     entry_points={
